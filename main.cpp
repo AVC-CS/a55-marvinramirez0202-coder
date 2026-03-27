c0225ddd20d8f5775ade5e168d788a495b376a82
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-03-27 15:52:45 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -14,17 +14,26 @@ int main()
 	cin >> M;
 
 	// TODO: declare variables for student name, score, sum, average
+	string sname;
+	int sum =0;
+	int score;
+	double avg;
 
 	for(int i=0; i<N; i++)
 	{
 		// TODO: read student name and initialize sum
-
+		cin >> sname;
+		sum = 0;
 		for (int j=0; j<M; j++)
 		{
+			cin >> score;
+			sum += score;
 			// TODO: read each score and accumulate sum
 		}
 
 		// TODO: calculate average and print name, sum, average
+		avg = sum / (double)M;
+		cout << sname << " " << sum << " " << avg << endl;
 	}
 
 	return 0;
